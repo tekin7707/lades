323d7a06bbdb8e1ff31a75ab7929d070e4093122
Musa Cavus <tekin7707@gmail.com>
2018-12-20 18:56:57 +0300

    Project Start

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private router : Router) {
+
+   }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.router.navigate(['dashboard']);
+  }
+}
